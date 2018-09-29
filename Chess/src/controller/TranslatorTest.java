019dce2dcd1679be0d6705e41988e1bb294e5c74
David Renner <david.renner@uni-ulm.de>
2018-09-30 00:34:31 +0200

    ...

--- Chess/src/controller/TranslatorTest.java
+++ Chess/src/controller/TranslatorTest.java
@@ -3,16 +3,17 @@ package controller;
 import model.Point;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 class TranslatorTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     void getAlgebraicNotation() {
         assertEquals("f2", Translator.getAlgebraicNotation(6,5));
         assertEquals("f2", Translator.getAlgebraicNotation(new Point(6,5)));
-        assertFalse("a1" == Translator.getAlgebraicNotation(0,0));
-        assertFalse("a1" == Translator.getAlgebraicNotation(new Point(0,0)));
+        assertFalse("a1".equals(Translator.getAlgebraicNotation(0,0)));
+        assertFalse("a1".equals(Translator.getAlgebraicNotation(new Point(0,0))));
     }
 
 }
